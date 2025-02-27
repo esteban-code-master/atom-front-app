0b682758e9e5ee19113a5cd38b159647d7909b7a
estebanxid <128723877+estebanxid@users.noreply.github.com>
2025-02-27 00:44:58 -0500

    feat: se creo opcion en el navbar para cerrar session

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import {
-	Component
-} from "@angular/core";
+import { Component } from "@angular/core"
+import { Router } from "@angular/router"
+import { SessionRepository } from "@module/auth/domain/repository/session.repository"
 
 @Component({
 	selector: "app-navbar",
 	templateUrl: "./navbar.component.html",
-	standalone: false
+	standalone: false,
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+	constructor(
+		private readonly sessionRepository: SessionRepository,
+		private readonly router: Router,
+	) {}
+
+	async signOut() {
+		await this.sessionRepository.signOut()
+
+		this.router.navigate(["/login"])
+	}
+}
