3ffea81bb4bdb58a48ebdc6dbec784951c7af236
estebanxid <128723877+estebanxid@users.noreply.github.com>
2025-02-27 01:01:53 -0500

    feat: se integro boton con spiner para creacion de usuario

--- src/app/modules/auth/presentation/components/create-user/create-user.component.ts
+++ src/app/modules/auth/presentation/components/create-user/create-user.component.ts
@@ -1,14 +1,20 @@
 import { Component, Inject } from "@angular/core"
+import { MatButtonModule } from "@angular/material/button"
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog"
 import { Router } from "@angular/router"
 import { CreateUserUseCase } from "@module/auth/application/use-case/create-user.use-case"
+import { ButtonSyncComponent } from "@shared/components/button-sync/button-sync.component"
 import { SnackBar } from "@shared/components/snackbar/snackbar.service"
 
 @Component({
 	selector: "app-confirm-dialog",
 	templateUrl: "./create-user.component.html",
+	imports: [MatButtonModule, ButtonSyncComponent],
+	standalone: true,
 })
 export class CreateUserComponent {
+	isLoading = false
+
 	constructor(
 		private createUserCase: CreateUserUseCase,
 		public dialogRef: MatDialogRef<CreateUserComponent>,
@@ -19,14 +25,17 @@ export class CreateUserComponent {
 
 	async onConfirm(): Promise<void> {
 		try {
+			this.isLoading = true
 			const user = await this.createUserCase.execute(this.data.email)
 
+			this.isLoading = false
 			localStorage.setItem("USER_ID", user.uid.toString())
 			this.dialogRef.close()
 			this.router.navigate(["/tasks"])
-			this.snackBar.showSuccess("usuario creado correctamente")
+			this.snackBar.showSuccess("Usuario creado correctamente")
 		} catch (error: unknown) {
-			this.snackBar.showSuccess("ha ocurrido un error al crear el usuarios")
+			this.dialogRef.close()
+			this.snackBar.showError("Ha ocurrido un error al crear el usuarios")
 		}
 	}
 
